63f72e2d8d37307d0807903ea22fcaf55fce0480
Douglas Osorio <douglas.anderson7@gmail.com>
2019-02-05 19:37:51 -0200

    Colocando pecas no tabuleiro

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -1,7 +1,10 @@
 package xadrez;
 
+import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
-import xadrez.PecaXadrez;;
+import xadrez.PecaXadrez;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;;
 
 public class PartidaXadrez {
 
@@ -9,6 +12,7 @@ public class PartidaXadrez {
 
 	public PartidaXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
+		configuracaoInicial();
 	}
 	
 	public PecaXadrez[][] getPecas (){
@@ -19,6 +23,12 @@ public class PartidaXadrez {
 			}
 		}
 		return mat;
+	}
+	
+	private void configuracaoInicial(){
+		tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao(2, 1));		
+		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(0, 4));
+		tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.BRANCO), new Posicao(7, 4));
 	}
 	
 }
